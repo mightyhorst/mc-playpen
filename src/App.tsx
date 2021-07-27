3f2e46896a3172a44497ef3a6d8b9085d7ddd35d
Nick Mitchell <nickymitch@gmail.com>
2021-07-27 11:37:28 +1000

    chore: init commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App({children}: {children?: ReactNode}) {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>mc playpen</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
