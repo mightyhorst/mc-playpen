aab66bd89c6cc801ac3753cf519c76b1c17e86ca
Nick Mitchell <nickymitch@gmail.com>
2021-07-27 16:24:44 +1000

    feat(workerize): add useWorker web worker hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import {ReactNode, useEffect, useState} from 'react';
 import "./styles.css";
 
 /**
+ * @requires WebWorkers
+ */
+import { 
+  useWorker,
+  WORKER_STATUS, 
+} from "@koale/useworker";
+
+/**
  * 
  */
 import {
@@ -16,10 +24,31 @@ import {
 } from './components';
 
 
+const numbers = [...Array(5000000)].map(e => ~~(Math.random() * 1000000));
+const sortNumbers = (nums:number[]) => nums.sort();
+
 export default function App({children}: {children?: ReactNode}) {
+
+  const [
+    sortWorker,
+  ] = useWorker(sortNumbers);
+
+  const [result, setResult] = useState<number[]>([]);
+
+  const runSort = async () => {
+    const result = await sortWorker(numbers); // non-blocking UI
+    setResult(result);
+  };
 
   return (
     <RecoilRoot>
+      <pre>
+        {JSON.stringify(WORKER_STATUS, null, 4)}
+        {JSON.stringify(result, null, 4)}
+      </pre>
+      <button type="button" onClick={runSort}>
+        Run Sort
+      </button>
       <div className="App">
         <TodoList />
       </div>
